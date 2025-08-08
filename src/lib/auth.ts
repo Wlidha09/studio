b3fa7c345201f1cef84eb986eb3ffdf08b96ef61
Adel Cherif <wlidha09@gmail.com>
2025-08-08 08:58:52 +0000

    add function signInWithPopup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,22 +1,32 @@
 
 "use client";
 
-import { getAuth, signInWithPopup, GoogleAuthProvider, User, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, User, signInWithEmailAndPassword, createUserWithEmailAndPassword, AuthProvider } from "firebase/auth";
 import { app } from "./firebase";
 
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
   'webClientId': '913952811592-7rmagrurc80vs2404g01d028btblicra.apps.googleusercontent.com'
 });
 
 
 export async function signInWithGoogle(): Promise<User | null> {
     try {
-        const result = await signInWithPopup(auth, provider);
+        const result = await signInWithPopup(auth, googleProvider);
         return result.user;
     } catch (error) {
         console.error("Error signing in with Google: ", error);
+        throw error;
+    }
+}
+
+export async function signInWithPopupGeneric(provider: AuthProvider): Promise<User | null> {
+    try {
+        const result = await signInWithPopup(auth, provider);
+        return result.user;
+    } catch (error) {
+        console.error("Error with popup sign-in: ", error);
         throw error;
     }
 }
